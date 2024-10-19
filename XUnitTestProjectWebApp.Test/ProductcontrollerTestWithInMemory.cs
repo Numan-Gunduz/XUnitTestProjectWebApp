7d0df72f6ca4688f8dc24c5472a49ef8455244ea
Numan-Gunduz <sanayiveteknoloji>
2024-10-19 13:52:14 +0300

    DeleteProductMetotduTesti

--- XUnitTestProjectWebApp.Test/ProductcontrollerTestWithInMemory.cs
+++ XUnitTestProjectWebApp.Test/ProductcontrollerTestWithInMemory.cs
@@ -11,7 +11,7 @@ using XUnitTestProjectWebApp.Models;
 
 namespace XUnitTestProjectWebApp.Test
 {
-    public class ProductcontrollerTestWithInMemory: ProductControllerTest
+    public class ProductcontrollerTestWithInMemory : ProductControllerTest
     {
         public ProductcontrollerTestWithInMemory()
         {
@@ -21,13 +21,13 @@ namespace XUnitTestProjectWebApp.Test
         [Fact]
         public async Task Create_ModelValidProduct_ReturnsTedirecToActionWithSaveProduct()
         {
-            var newproduct = new Product { ProductName = "klav", ProductPrice = 200, ProductStock = 210, ProductColor = "Mavil"};
+            var newproduct = new Product { ProductName = "klav", ProductPrice = 200, ProductStock = 210, ProductColor = "Mavil" };
 
             using (var context = new ProductContext(_contextOptions))
             {
 
                 var category = await context.Categories.FirstAsync();
-                newproduct.CategoryId=category.CategoryId;
+                newproduct.CategoryId = category.CategoryId;
                 var controller = new ProductsController(context);
                 var result = await controller.Create(newproduct);
                 var redirect = Assert.IsType<RedirectToActionResult>(result);
@@ -39,6 +39,25 @@ namespace XUnitTestProjectWebApp.Test
             {
                 var product = context.Products.FirstOrDefault(x => x.ProductName == newproduct.ProductName);
                 Assert.Equal(newproduct.ProductName, product.ProductName);
+            }
+        }
+        [Theory]
+        [InlineData(1)]
+        public async Task DeleteCategory_ExistCategoryId_DeleteALlProducts(int categoryId)
+        {
+            using (var context = new ProductContext(_contextOptions))
+            {
+                var category=await context.Categories.FindAsync(categoryId);
+
+                context.Categories.Remove(category);
+                context.SaveChanges();
+
+            }
+
+            using (var context = new ProductContext(_contextOptions))
+            {
+                var products = await context.Products.Where(x => x.CategoryId == categoryId).ToListAsync();
+                Assert.Empty(products);
             }
         }
     }
