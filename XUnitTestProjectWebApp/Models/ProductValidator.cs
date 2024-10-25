6b2e449be6b71dadd2ffb2910868a05028d05314
Numan-Gunduz <sanayiveteknoloji>
2024-10-25 13:58:55 +0300

    FluentValidation

---
+++ XUnitTestProjectWebApp/Models/ProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace XUnitTestProjectWebApp.Models
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+
+        {
+
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün İsmi Boş Geçilemez")
+                .Length(2, 50).WithMessage("Ürün İsmi 2-50 karakter arasında olması gerekiyor");
+            RuleFor(p => p.ProductPrice).GreaterThan(0).WithMessage("Ürünün Fiyatı 0 dan büyük olmak zorundadır");
+            RuleFor(p => p.ProductStock).GreaterThanOrEqualTo(0).WithMessage("Ürünün Stock Adedi 0'a eşit veya büyük olmak zorundadır");
+            RuleFor(p => p.CategoryId).NotNull().WithMessage("Ürüne Ait kategori Id alanı boş geçilmez");
+        }
+    }
+}
