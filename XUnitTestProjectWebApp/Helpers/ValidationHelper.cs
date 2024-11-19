94374d9a481a1367f84dfad727f116142214c98c
Numan-Gunduz <sanayiveteknoloji>
2024-11-19 09:53:56 +0300

    CustomizeII

---
+++ XUnitTestProjectWebApp/Helpers/ValidationHelper.cs
@@ -0,0 +1,47 @@
+﻿//using FluentValidation;
+//using FluentValidation.Results;
+//using XUnitTestProjectWebApp.Models;
+
+//namespace XUnitTestProjectWebApp.Helpers
+
+//{
+
+//    public static class ValidationHelper
+//    {
+//        public static void KosulSaglanirsaHataEkle(bool condition, string errorMessage, string propertyName, ValidationContext<Product> context)
+//        {
+//            if (condition)
+//            {
+//                context.AddFailure(new ValidationFailure(propertyName, errorMessage));
+//            }
+//        }
+//    }
+//}
+
+using FluentValidation;
+using FluentValidation.Results;
+using XUnitTestProjectWebApp.Models;
+using System;
+
+namespace XUnitTestProjectWebApp.Helpers
+{
+    public static class ValidationHelper
+    {
+        public static void HataFirlatVeEkle(Func<bool> condition, string errorMessage, string propertyName, ValidationContext<Product> context)
+        {
+            try
+            {
+                // Şart sağlanıyorsa throw fırlatmak için if'e gir
+                if (condition())
+                {
+                    throw new ValidationException(errorMessage);
+                }
+            }
+            catch (ValidationException ex)
+            {
+                // Hata durumunda hatayı context üzerine ekleyerek tüm kuralların devam etmesini sağla
+                context.AddFailure(new ValidationFailure(propertyName, ex.Message));
+            }
+        }
+    }
+}
--- XUnitTestProjectWebApp/Models/ProductValidator.cs
+++ XUnitTestProjectWebApp/Models/ProductValidator.cs
@@ -1,6 +1,37 @@
-﻿
+﻿//using FluentValidation;
+//using XUnitTestProjectWebApp.Helpers;
+
+//namespace XUnitTestProjectWebApp.Models
+//{
+//    public class ProductValidator : AbstractValidator<Product>
+//    {
+//        public ProductValidator()
+//        {
+//            RuleFor(p => p.ProductName)
+//                .NotEmpty().WithMessage("Ürün İsmi Boş Geçilemez")
+//                .Length(2, 50).WithMessage("Ürün İsmi 2-50 karakter arasında olması gerekiyor")
+//                .Custom((productName, context) =>
+//                    ValidationHelper.KosulSaglanirsaHataEkle(string.IsNullOrWhiteSpace(productName),
+//                    "Ürün ismi geçersizdir.", nameof(Product.ProductName), context));
+
+//            RuleFor(product => product.ProductPrice)
+//                .GreaterThan(0).WithMessage("Ürün fiyatı sıfırdan büyük olmalıdır.")
+//                .Custom((price, context) =>
+//                    ValidationHelper.KosulSaglanirsaHataEkle(price <= 0,
+//                    "Ürün fiyatı sıfırdan büyük olmalıdır.", nameof(Product.ProductPrice), context));
+
+//            RuleFor(p => p.ProductStock)
+//                .GreaterThanOrEqualTo(0).WithMessage("Ürünün Stock Adedi 0'a eşit veya büyük olmak zorundadır")
+//                .Custom((stock, context) =>
+//                    ValidationHelper.KosulSaglanirsaHataEkle(stock < 0,
+//                    "Ürün stoğu negatif olamaz.", nameof(Product.ProductStock), context));
+//        }
+
+
+//    }
+//}
 using FluentValidation;
-using FluentValidation.Results;
+using XUnitTestProjectWebApp.Helpers;
 
 namespace XUnitTestProjectWebApp.Models
 {
@@ -11,29 +42,21 @@ namespace XUnitTestProjectWebApp.Models
             RuleFor(p => p.ProductName)
                 .NotEmpty().WithMessage("Ürün İsmi Boş Geçilemez")
                 .Length(2, 50).WithMessage("Ürün İsmi 2-50 karakter arasında olması gerekiyor")
-                .Custom((productName, context) =>
-                    KosulSaglanirsaHataEkle(string.IsNullOrWhiteSpace(productName),
+                .Custom((productName, context) => ValidationHelper.HataFirlatVeEkle(() =>
+                    string.IsNullOrWhiteSpace(productName),
                     "Ürün ismi geçersizdir.", nameof(Product.ProductName), context));
 
             RuleFor(product => product.ProductPrice)
                 .GreaterThan(0).WithMessage("Ürün fiyatı sıfırdan büyük olmalıdır.")
-                .Custom((price, context) =>
-                    KosulSaglanirsaHataEkle(price <= 0,
+                .Custom((price, context) => ValidationHelper.HataFirlatVeEkle(() =>
+                    price <= 0,
                     "Ürün fiyatı sıfırdan büyük olmalıdır.", nameof(Product.ProductPrice), context));
 
             RuleFor(p => p.ProductStock)
-                .GreaterThanOrEqualTo(0).WithMessage("Ürünün Stock Adedi 0'a eşit veya büyük olmak zorundadır")
-                .Custom((stock, context) =>
-                    KosulSaglanirsaHataEkle(stock < 0,
+                .GreaterThanOrEqualTo(0).WithMessage("Ürünün Stok Adedi 0'a eşit veya büyük olmak zorundadır")
+                .Custom((stock, context) => ValidationHelper.HataFirlatVeEkle(() =>
+                    stock < 0,
                     "Ürün stoğu negatif olamaz.", nameof(Product.ProductStock), context));
-        }
-
-        private void KosulSaglanirsaHataEkle(bool condition, string errorMessage, string propertyName, ValidationContext<Product> context)
-        {
-            if (condition)
-            {
-                context.AddFailure(new ValidationFailure(propertyName, errorMessage));
-            }
         }
     }
 }
